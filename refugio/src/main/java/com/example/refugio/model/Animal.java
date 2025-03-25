fbd574d97fa18abff6c67c8bfdb2d64fc99934a9
CSantiagoPorto <148074839+CSantiagoPorto@users.noreply.github.com>
2025-03-25 12:22:07 +0100

    Creado proyecto y model

---
+++ refugio/src/main/java/com/example/refugio/model/Animal.java
@@ -0,0 +1,21 @@
+package com.example.refugio.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.io.Serializable;
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name= "animales")
+
+public class Animal implements Serializable {
+    private String nombre;
+
+}
