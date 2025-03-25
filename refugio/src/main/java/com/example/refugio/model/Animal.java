1f489ccc914f5f8c645b8171ac5e65d31d85af0d
CSantiagoPorto <148074839+CSantiagoPorto@users.noreply.github.com>
2025-03-25 12:50:22 +0100

    Porblema dependencias solucionado

--- refugio/src/main/java/com/example/refugio/model/Animal.java
+++ refugio/src/main/java/com/example/refugio/model/Animal.java
@@ -1,11 +1,14 @@
 package com.example.refugio.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import java.util.Date;
+import jakarta.persistence.Table;
+import jakarta.persistence.TemporalType;
+
 
 import java.io.Serializable;
 @Getter
@@ -16,6 +19,26 @@ import java.io.Serializable;
 @Table(name= "animales")
 
 public class Animal implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    @Column
     private String nombre;
+    @Column
+    private String especie;
+    @Column
+    private String raza;
+    @Column
+    private int edad;
+    @Column
+    private  boolean vacunado;
+    @Column(name="fecha_ingreso")
+    @Temporal(TemporalType.DATE)
+    private Date fechaIngreso;
 
+    public Animal(String raza, String especie, String nombre) {
+        this.raza = raza;
+        this.especie = especie;
+        this.nombre = nombre;
+    }
 }
