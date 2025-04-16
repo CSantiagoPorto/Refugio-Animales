5a73ab6b5ba13360638aaf41c37a96cd911763b6
CSantiagoPorto <148074839+CSantiagoPorto@users.noreply.github.com>
2025-04-16 19:07:37 +0200

    Creado método obtenerAnimalesDeCuidador, pendiente de implementación en la clase CuidadorController

--- refugio/src/main/java/com/example/refugio/services/CuidadorService.java
+++ refugio/src/main/java/com/example/refugio/services/CuidadorService.java
@@ -1,9 +1,15 @@
 package com.example.refugio.services;
 
+import com.example.refugio.model.Animal;
 import com.example.refugio.model.Cuidador;
+import com.example.refugio.repository.CuidadorRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface CuidadorService {
     Optional<Cuidador>buscarCuidadorId(int id);
+    List<Cuidador>obternerTodosCuidadores();
+    Cuidador añadirCuidador(Cuidador cuidador);
+    List<Animal>obtenerAnimalesDeCuidador(int id);
 }
--- refugio/src/main/java/com/example/refugio/services/implementacion/CuidadorServiceImp.java
+++ refugio/src/main/java/com/example/refugio/services/implementacion/CuidadorServiceImp.java
@@ -1,11 +1,16 @@
 package com.example.refugio.services.implementacion;
 
+import com.example.refugio.model.Animal;
 import com.example.refugio.model.Cuidador;
 import com.example.refugio.repository.CuidadorRepository;
 import com.example.refugio.services.CuidadorService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
+import java.util.List;
 import java.util.Optional;
 @Service
 
@@ -15,5 +20,27 @@ public class CuidadorServiceImp implements CuidadorService {
     @Override
     public Optional<Cuidador> buscarCuidadorId(int id) {
         return Optional.empty();
+    }
+
+    @Override
+    public List<Cuidador> obternerTodosCuidadores() {
+        return cuidadorRepository.findAll();
+        //Cuidador repository tiene el método ya hecho
+    }
+
+    @Override
+    public Cuidador añadirCuidador(Cuidador cuidador) {
+        return cuidadorRepository.save(cuidador);
+    }
+
+    @Override
+    public List<Animal> obtenerAnimalesDeCuidador(int id) {
+        //Creo un objeto Cuidador, este tiene como atributo los animales asignados.
+        //Al igualar el objeto a cuidadorRepository, cuidador pueded acceder a los métodos de JPARepository
+        //Si falla, porque ese id no existe, lanza un 404
+        //Si no devuelve una lista con los animales asignados
+        Cuidador cuidador=cuidadorRepository.findById(id)
+                .orElseThrow(()-> new ResponseStatusException(HttpStatus.NOT_FOUND, "Cuidador no encontrado"));
+        return cuidador.getAnimales();
     }
 }
